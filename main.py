576d4927a9beb687f542548d6446e7abd94dd598
LULLULALLA0525 <t.wooo@kakao.com>
2022-09-07 03:15:53 +0000

    구명보트 트라이

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 def solution(people, limit):
-    answer = 0
-    rescued = [False for _ in range(len(people))]
+	answer = 0
+	too_heavy = []
 
-    remain = limit
-    for i in range(len(people) - 1, -1, -1):
-        if rescued[i]:
-            continue
+	while people:
+		remain = limit
+		is_boat_full = False
 
+		while not is_boat_full:
+			if people:
+				person = people.pop()
+			else:
+				is_boat_full = True
+				break
+			while remain < person:
+				too_heavy.append(person)
+				if people:
+					person = people.pop()
+				else:
+					is_boat_full = True
+					break
+		
+			while too_heavy:
+				people.append(too_heavy.pop())
+		
+			if not is_boat_full:
+				remain -= person
 
-    return answer
+			if remain == 0:
+				is_boat_full = True
+
+		answer += 1
+
+	return answer
 
 people = [70, 80, 50, 50]
 limit = 100
