76313bb6333f8866d59ef8add621f9fc17ef6372
LULLULALLA0525 <t.wooo@kakao.com>
2022-09-18 04:06:40 +0000

    보석구매

--- main.py
+++ main.py
@@ -1,53 +1,32 @@
-from collections import deque
+def solution(gems):
+	gem_list = {}
+	for gem_idx in range(len(gems)):
+		if gems[gem_idx] in gem_list.keys():
+			gem_list[gems[gem_idx]].append(gem_idx)
+		else:
+			gem_list[gems[gem_idx]] = []
+			gem_list[gems[gem_idx]].append(gem_idx)
+	print(gem_list)
 
-def rescue_heavy(people_queue, remain):
-	heaviest = people_queue.pop()
-	if remain >= heaviest:
-		return heaviest
-	else:
-		people_queue.append(heaviest)
-		return 0
-
-def rescue_light(people_queue, remain):
-	lightest = people_queue.popleft()
-	if remain >= lightest:
-		return lightest
-	else:
-		people_queue.appendleft(lightest)
-		return 0
-	
-
-def solution(people, limit):
-	answer = 0
-	people.sort()
-	people_queue = deque(people)
-
-	while people_queue:
-		remain = limit
-		while True:
-			if people_queue:
-				rescued = rescue_heavy(people_queue, remain)
-				remain -= rescued
-			else:
-				if remain != limit:
-					answer += 1
-				break
-
-			if people_queue:
-				rescued = rescue_light(people_queue, remain)
-				if rescued == 0:
-					answer += 1
-					break
-				else:
-					remain -= rescued
-			else:
-				if remain != limit:
-					answer += 1
-				break
-	
+	start_idx, end_idx = 0, 0
+	answer = [start_idx, end_idx]
 	return answer
 
-people = [70, 80, 50, 50]
-limit = 100
+gems = list(input().split())
+#DIA RUBY RUBY DIA DIA EMERALD SAPPHIRE DIA
+#AA AB AC AA AC
+#XYZ XYZ XYZ
+#ZZZ YYY NNNN YYY BBB
 
-print(solution(people, limit))
+print(solution(gems))
+#[3, 7]
+#[1, 3]
+#[1, 1]
+#[1, 5]
+
+#0 1 2 3 4 5 6 7
+#1 2 3 1 2 2 3 1
+#1 -> 0, 3, 7
+#2 -> 1, 4, 5
+#3 -> 2, 6
+print("태우야 사랑해")
