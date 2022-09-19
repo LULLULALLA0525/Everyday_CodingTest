d702bcf72b09a412ebb80a640a37bf8eb595da04
Tae Woo <t.wooo@kakao.com>
2022-09-19 20:16:05 +0900

    보석 구매

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 def solution(gems):
     gem_dict = {}
-    for gem_idx in range(len(gems)):
-        if gems[gem_idx] in gem_dict.keys():
-            gem_dict[gems[gem_idx]].append(gem_idx + 1)
-        else:
-            gem_dict[gems[gem_idx]] = []
-            gem_dict[gems[gem_idx]].append(gem_idx + 1)
-    print(gem_dict)
 
-    first_gem_index = []
-    for gem in gem_dict.keys():
-        first_gem_index.append(gem_dict[gem][0])
-    min_index = max(first_gem_index)
 
     start_idx, end_idx = 0, 0
     answer = [start_idx, end_idx]
@@ -29,10 +18,3 @@ print(solution(gems_temp))
 # [1, 3]
 # [1, 1]
 # [1, 5]
-
-# 1 2 3 4 5 6 7 8
-# 1 2 3 1 2 2 3 1
-# 1 -> 1, 4, 8
-# 2 -> 2, 5, 6
-# 3 -> 3, 7
-print("태우야 사랑해")
