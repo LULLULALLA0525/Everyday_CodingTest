1be6431d7d59b3bbbda2713ecd0f3531f54be462
Tae Woo <t.wooo@kakao.com>
2022-09-28 22:50:12 +0900

    파일 합치기

---
+++ 파일 합치기.py
@@ -0,0 +1,19 @@
+# 파일을 합치는 데 소요되는 시간은 두 파일의 크기의 합과 같다.
+# 파일의 크기들이 나열될 떄, 파일을 전부 합칠 수 있는 최소 비용을 구하시오.
+def solution(n, files):
+    
+    return 0
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    files = list(map(int, input().split()))
+    print(solution(n, files))
+
+
+# 2
+# 4
+# 40 30 30 50
+# 15
+# 1 21 3 4 5 35 5 4 3 5 98 21 14 17 32
