3eca58d938d232e3bd5fd42cf7e6441f50bab1fc
Tae Woo <t.wooo@kakao.com>
2022-11-05 00:39:31 +0900

    0

---
+++ 이항 계수 1.py
@@ -0,0 +1,20 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+def solution(n, k):
+    large = max([n, k])
+    small = min([n, k])
+
+    value = 1
+    for i in range(small):
+        value *= (large - i)
+    fact = factorial(small)
+    return value // fact
+
+
+N, K = list(map(int, input().split()))
+print(solution(N, K))
