9880130401fa6b07e1fbc2408426858f15554fe1
Tae Woo <t.wooo@kakao.com>
2022-11-05 18:06:56 +0900

    팩토리얼 0의 개수

--- 팩토리얼 0의 개수.py
+++ 팩토리얼 0의 개수.py
@@ -1,5 +1,18 @@
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
 def solution(n):
-    return n
+    result = factorial(n)
+    cnt = 0
+    while result % 10 == 0:
+        cnt += 1
+        result //= 10
+
+    return cnt
 
 
 n = int(input())
