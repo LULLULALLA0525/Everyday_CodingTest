14a1374ae09f101c1addfc6518c211a3aea8e0c8
김태우 <kim.taewoo@mintech.kr>
2023-11-11 12:02:14 +0900

    타노스

---
+++ 타노스.py
@@ -0,0 +1,24 @@
+import sys
+
+input = lambda: sys.stdin.readline()
+
+def solution(s):
+  counts = [0, 0]
+  for c in s:
+    counts[c] += 1
+  
+  new_s = []
+  new_counts = [0, 0]
+  for c in s:
+    new_counts[c] += 1
+    if c == 0:
+      if new_counts[0] <= counts[0] // 2:
+        new_s.append("0")
+    else:
+      if new_counts[1] > counts[1] // 2:
+        new_s.append("1")
+
+  return "".join(new_s)
+
+S = list(map(int, list(input().strip())))
+print(solution(S))
