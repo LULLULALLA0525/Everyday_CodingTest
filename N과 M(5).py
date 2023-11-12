6c4ad1670c041d9fec4e7c66cb183bf4d7635af8
김태우 <kim.taewoo@mintech.kr>
2023-11-12 13:29:31 +0900

    N과 M(5)

---
+++ N과 M(5).py
@@ -0,0 +1,14 @@
+import sys
+from itertools import permutations
+
+input = lambda: sys.stdin.readline()
+
+def solution(n, m, numbers):
+  numbers = sorted(numbers)
+  results = permutations(numbers, m)
+  for result in results:
+    print(*result)
+
+N, M = list(map(int, input().split()))
+NUMBERS = list(map(int, input().split()))
+solution(N, M, NUMBERS)
