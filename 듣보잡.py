54e94003550253179bd80ba7a14692201b8cee2d
TAE-COLA <t.wooo@kakao.com>
2024-07-10 16:56:13 +0900

    듣보잡

--- 듣보잡.py
+++ 듣보잡.py
@@ -1,22 +1,25 @@
+import heapq
 import sys
-input = sys.stdin.readline
-print = sys.stdout.write
+from collections import defaultdict
 
+input = lambda: sys.stdin.readline().rstrip()
 
-D, B = list(map(int, input().split()))
-D_LIST = []
-for _ in range(D):
-    D_LIST.append(input().split("\n")[0])
+def solution(deutdo, bodo):
+  DEUTDO_BODO = []
+  for name in bodo:
+    if DEUTDO[name]:
+      heapq.heappush(DEUTDO_BODO, name)
+  
+  length = len(DEUTDO_BODO)
+  print(length)
+  for _ in range(length):
+    print(heapq.heappop(DEUTDO_BODO))
 
-DB_LIST = []
-cnt = 0
-for _ in range(B):
-    name = input().split("\n")[0]
-    if name in D_LIST:
-        DB_LIST.append(name)
-        cnt += 1
-
-print(str(cnt))
-temp = sorted(DB_LIST)
-for i in range(cnt):
-    print(temp[i])
+N, M = list(map(int, input().split()))
+DEUTDO = defaultdict(bool)
+for _ in range(N):
+  DEUTDO[input()] = True
+BODO = []
+for _ in range(M):
+  BODO.append(input())
+solution(DEUTDO, BODO)
