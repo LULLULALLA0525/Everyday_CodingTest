68f741fd73f87d70f207a08d81bc7b4052fa1db8
TAE-COLA <t.wooo@kakao.com>
2024-07-10 16:43:59 +0900

    분산처리

--- 분산처리.py
+++ 분산처리.py
@@ -1,17 +1,20 @@
 import sys
 
-input = lambda: sys.stdin.readline()
+input = lambda: sys.stdin.readline().rstrip()
 
-
-def solution(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-        result %= 10
+# a^b의 1의 자릿수를 출력하는 함수
+def getLastDigitAfterPower(a, b):
+  # 거듭제곱은 4를 주기로 반복된다.
+  result = a ** (b % 4 + 4) % 10
+  if result == 0:
+    return 10
+  else:
     return result
 
-
 T = int(input())
-for i in range(T):
-    A, B = list(map(int, input().split()))
-    print(solution(A, B))
+ANSWERS = []
+for _ in range(T):
+  A, B = list(map(int, input().split()))
+  ANSWERS.append(getLastDigitAfterPower(A % 10, B))
+for ANSWER in ANSWERS:
+  print(ANSWER)
