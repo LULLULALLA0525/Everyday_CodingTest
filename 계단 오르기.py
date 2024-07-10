258035a63ef3092dfaec1f4738d177059b81e67c
TAE-COLA <t.wooo@kakao.com>
2024-07-10 17:28:58 +0900

    계단 오르기

--- 계단 오르기.py
+++ 계단 오르기.py
@@ -1,27 +1,20 @@
 import sys
 
-input = lambda: sys.stdin.readline()
-
+input = lambda: sys.stdin.readline().rstrip()
 
 def solution(n, stairs):
-    dp = [[0, 0], [0, stairs[1]], [1, stairs[1] + stairs[2]]]
-    for i in range(3, n + 1):
-        dp.append([0, 0])
-        if dp[i - 1][0] == 1:
-            dp[i][0] = 0
-            dp[i][1] = dp[i - 2][1] + stairs[i]
-        else:
-            if dp[i - 1][1] > dp[i - 2][1]:
-                dp[i][0] = 1
-                dp[i][1] = dp[i - 1][1] + stairs[i]
-            else:
-                dp[i][0] = 0
-                dp[i][1] = dp[i - 2][1] + stairs[i]
-    return dp[n][1]
+  dp = [(0, 0) for _ in range(n)]   #(직전 칸에서 올라온 경우, 두 칸 아래서 올라온 경우)
+  dp[0] = (stairs[0], stairs[0])
+  if n > 1:
+    dp[1] = (stairs[0] + stairs[1], stairs[1])
 
+  for i in range(2, n):
+    dp[i] = (dp[i - 1][1] + stairs[i], max(dp[i - 2]) + stairs[i])
+  
+  return max(dp[n - 1])
 
 N = int(input())
-STAIRS = [0]
+STAIRS = []
 for _ in range(N):
-    STAIRS.append(int(input()))
+  STAIRS.append(int(input()))
 print(solution(N, STAIRS))
