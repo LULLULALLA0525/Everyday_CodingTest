412ff7459c24d737bfbe136afb6ccbac65ac53ed
TAE-COLA <t.wooo@kakao.com>
2024-07-10 17:58:34 +0900

    유기농 배추

--- 유기농 배추.py
+++ 유기농 배추.py
@@ -1,35 +1,43 @@
 import sys
 from collections import deque
 
-input = lambda: sys.stdin.readline()
+input = lambda: sys.stdin.readline().rstrip()
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx = [ 0, 0, -1, 1]
+dy = [-1, 1,  0, 0]
 
+def solution(n, m, farm):
+  bugs = 0
+
+  for row in range(n):
+    for column in range(m):
+      if farm[row][column]:
+        bugs += 1
+
+        queue = deque()
+        farm[row][column] = False
+        queue.append((column, row))
+
+        while(queue):
+          x, y = queue.popleft()
+          for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or ny < 0 or nx >= m or ny >= n:
+              continue
+            if farm[ny][nx]:
+              farm[ny][nx] = False
+              queue.append((nx, ny))
+
+  return bugs
 
 T = int(input())
+ANSWERS = []
 for _ in range(T):
-    M, N, K = list(map(int, input().split()))
-    FARM = [[0] * M for _ in range(N)]
-    VISITED = [[False] * M for _ in range(N)]
-    for _ in range(K):
-        X, Y = list(map(int, input().split()))
-        FARM[Y][X] = 1
-    answer = 0
-    queue = deque()
-    for y in range(N):
-        for x in range(M):
-            if FARM[y][x] == 1 and not VISITED[y][x]:
-                answer += 1
-                queue.append((x, y))
-                while queue:
-                    xx, yy = queue.popleft()
-                    VISITED[yy][xx] = True
-                    for i in range(4):
-                        nx = xx + dx[i]
-                        ny = yy + dy[i]
-                        if nx < 0 or nx >= M or ny < 0 or ny >= N:
-                            continue
-                        if FARM[ny][nx] == 1 and not VISITED[ny][nx]:
-                            queue.append((nx, ny))
-    print(answer)
+  M, N, K = list(map(int, input().split()))
+  FARM = [[False for _ in range(M)] for _ in range(N)]
+  for _ in range(K):
+    X, Y = list(map(int, input().split()))
+    FARM[Y][X] = True
+  ANSWERS.append(solution(N, M, FARM))
+for ANSWER in ANSWERS:
+  print(ANSWER)
