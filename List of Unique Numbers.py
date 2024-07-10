53abf6318b65e9dce8daaffd248b30eab92d0c27
TAE-COLA <t.wooo@kakao.com>
2024-07-10 20:19:25 +0900

    List of Unique Numbers

--- List of Unique Numbers.py
+++ List of Unique Numbers.py
@@ -1,21 +1,21 @@
 import sys
+from collections import defaultdict
 
 input = lambda: sys.stdin.readline()
 
-def solution(numbers):
-  max_num = max(numbers)
-  result = 0
-  for start in range(len(numbers)):
-    isShown = [False for _ in range(max_num + 1)]
-    for end in range(start, len(numbers)):
-      if isShown[numbers[end]]:
-        break
-      else:
-        result += 1
-        isShown[numbers[end]] = True
+def solution(n, numbers):
+  reachableToLeft = []
+  lastAppeared = defaultdict(lambda: -1)
+  for i in range(n):
+    if i == 0:
+      reachableToLeft.append(1)
+    else:
+      mine = i - lastAppeared[numbers[i]]
+      reachableToLeft.append(min(mine, reachableToLeft[i - 1] + 1))
+    lastAppeared[numbers[i]] = i
 
-  return result
+  return sum(reachableToLeft)
 
 N = int(input())
 NUMBERS = list(map(int, input().split()))
-print(solution(NUMBERS))
+print(solution(N, NUMBERS))
