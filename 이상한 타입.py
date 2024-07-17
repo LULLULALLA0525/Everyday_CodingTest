7c76032ea89eb2e57715569b86923a2e8a30af4b
TAE-COLA <t.wooo@kakao.com>
2024-07-17 19:09:46 +0900

    이상한 타입

---
+++ 이상한 타입.py
@@ -0,0 +1,11 @@
+import sys
+
+input = lambda: sys.stdin.readline()
+
+def solution(types, codes):
+    answer = []
+    return answer
+
+TEST_TYPES = []
+TEST_CODES = []
+print(solution(TEST_TYPES, TEST_CODES))
