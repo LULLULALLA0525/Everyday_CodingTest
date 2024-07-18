3495a75589dfc888448c93a9a5812ac0c97e7beb
TAE-COLA <t.wooo@kakao.com>
2024-07-18 22:22:04 +0900

    이상한 타입 테스트 케이스 작성

--- 이상한 타입.py
+++ 이상한 타입.py
@@ -4,8 +4,30 @@ input = lambda: sys.stdin.readline()
 
 def solution(types, codes):
     answer = []
+    errors = 0
+
+    
+
+    answer += [errors]
     return answer
 
-TEST_TYPES = []
-TEST_CODES = []
-print(solution(TEST_TYPES, TEST_CODES))
+# a: 1~9, s: 1~4, b: 5~9
+TEST_TYPES = [
+    "atype aaba",
+    "btype assa",
+    "ctype asba"
+]
+TEST_CODES = [
+    "declare atype a 1845",     # a <- 1845
+    "declare btype b 8923",     # error
+    "print a",                  # print 1845
+    "print b",                  # error
+    "declare ctype c 3329",     # c <- 3329
+    "update b 5229",            # error
+    "update a c",               # a <- 3329(c)
+    "update c 2381",            # c <- 2381
+    "print a",                  # print 3329
+    "print b",                  # error
+    "print c"                   # print 2381
+]
+print(solution(TEST_TYPES, TEST_CODES))     # [1845, 3329, 2381, 4]
